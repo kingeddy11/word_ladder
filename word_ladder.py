8f5bfc65d3740c1a0eb1da38941469ca8ebe6127
Edward <Edward.Wu.23@cmc.edu>
2021-02-19 00:27:44 -0800

    I've passed all the test cases for word ladder.

--- word_ladder.py
+++ word_ladder.py
@@ -1,9 +1,12 @@
 #!/bin/python3
+from collections import deque
+import copy
 
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
     Returns a list satisfying the following properties:
+
 
     1. the first element is `start_word`
     2. the last element is `end_word`
@@ -16,18 +19,45 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     ```
     may give the output
     ```
-    ['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny', 'boney', 'money']
+    ['stone', 'shone', 'phone', 'phony', 'peony', 'penny', 'benny', 'bonny',
+    'boney', 'money']
     ```
     but the possible outputs are not unique,
     so you may also get the output
     ```
-    ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey', 'honey', 'money']
+    ['stone', 'shone', 'shote', 'shots', 'soots', 'hoots', 'hooty', 'hooey',
+    'honey', 'money']
     ```
     (We cannot use doctests here because the outputs are not unique.)
-
-    Whenever it is impossible to generate a word ladder between the two words,
+    Whenever it is impossible to generate a word ladder between the two
+    words,
     the function returns `None`.
     '''
+    wordslist = []
+    if(start_word == end_word):
+        return [start_word]
+    if len(start_word) != len(end_word):
+        return None
+    with open(dictionary_file, 'r') as x:
+        for word in x.readlines():
+            wordslist.append(word.strip('\n'))
+    list1 = []
+    list1.append(start_word)
+    q1 = deque()
+    q1.append(list1)
+
+    while len(q1) > 0:
+        mainstack = q1.popleft()
+        for word in set(wordslist):
+            if _adjacent(word, mainstack[-1]):
+                if word == end_word:
+                    mainstack.append(word)
+                    return mainstack
+                newstack = copy.deepcopy(mainstack)
+                newstack.append(word)
+                q1.append(newstack)
+                wordslist.remove(word)
+    return None
 
 
 def verify_word_ladder(ladder):
@@ -40,6 +70,15 @@ def verify_word_ladder(ladder):
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
+    if not ladder:
+        return False
+    balanced = True
+    for i in range(0, len(ladder) - 1):
+        if _adjacent(ladder[i], ladder[i+1]):
+            balanced = True
+        else:
+            return False
+    return balanced
 
 
 def _adjacent(word1, word2):
@@ -52,3 +91,13 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    if len(word1) != len(word2) or word1 == word2:
+        return False
+    x = 0
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            x += 1
+    if x == 1:
+        return True
+    else:
+        return False
